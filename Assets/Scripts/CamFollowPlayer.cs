9655afb546cffcff0785ce70bfca25c20f315b5c
Merve Nur Erdem <mervenurerdem@hotmail.com>
2023-06-07 21:17:59 +0300

    Camera Settings Added
    CameraFollow eklendi

---
+++ Assets/Scripts/CamFollowPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowPlayer : MonoBehaviour
+{
+    public Transform cameraTarget;
+    public float sSpeed;
+    public Vector3 dist;
+    public Transform lookTarget;
+
+    private void LateUpdate()
+    {
+        Vector3 dPos = cameraTarget.position + dist;
+        Vector3 sPos = Vector3.Lerp(transform.position, dPos, sSpeed * Time.deltaTime);
+        transform.position = sPos;
+        transform.LookAt(lookTarget.position);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
